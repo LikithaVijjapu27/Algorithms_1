5070afc1f1dce364dff6660aa57df9b5bfed8ed3
LikithaVijjapu <likithavijjapu@gmail.com>
2021-02-24 23:53:15 -0800

    merge 2

--- InterviewPrep/src/sudoku/solveSudoku.java
+++ InterviewPrep/src/sudoku/solveSudoku.java
@@ -5,6 +5,12 @@ import java.io.*;
 public class solveSudoku {
 	public void printSudoku() {
 		System.out.println("solving sudoku");
+		System.out.println("size :"+ getSize(23));
+		
+	}
+	
+	private static int getSize(int n) {
+		return n+1;
 	}
 	
 }
