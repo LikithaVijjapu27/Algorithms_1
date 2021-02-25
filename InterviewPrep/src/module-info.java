2a33fee64e233bb9f67f7dba4982d81967e82cb0
LikithaVijjapu <likithavijjapu@gmail.com>
2021-02-24 23:05:02 -0800

    first commit

---
+++ InterviewPrep/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sgaruda
+ *
+ */
+module InterviewPrep {
+}
---
+++ InterviewPrep/src/sudoku/solveSudoku.java
@@ -0,0 +1,10 @@
+package sudoku;
+import java.io.*;
+
+
+public class solveSudoku {
+	public void printSudoku() {
+		System.out.println("solving sudoku");
+	}
+	
+}
